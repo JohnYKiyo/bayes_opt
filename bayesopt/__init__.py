32bad57088b929314588b272e5056905828e4e3c
JohnYKiyo <dummn.marionette.7surspecies@gmail.com>
2020-07-13 00:56:07 +0900

    [update] __init__

--- bayesopt/__init__.py
+++ bayesopt/__init__.py
@@ -1,4 +1,7 @@
 __all__=['kernel','metric','utils','GPR.GPR']
 from . import acquisition
+from . import acquisition_optimizer
 from .gp.gp import kernel,metric,utils
 from .gp.gp.GPR import GPR
+from .bo import BayesOpt
+from .plotter import plot_history
