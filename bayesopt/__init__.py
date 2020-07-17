ebd533c6ca1fe5ac9e4f4d5d9581632ea4327226
JohnYKiyo <dummn.marionette.7surspecies@gmail.com>
2020-07-17 15:13:55 +0900

    [upgrade]Stop to using submodule "gp". use https://github.com/JohnYKiyo/GaussianProcess.git

--- bayesopt/__init__.py
+++ bayesopt/__init__.py
@@ -1,7 +1,5 @@
-__all__=['kernel','metric','utils','GPR.GPR']
+from GaussianProcess import *
 from . import acquisition
 from . import acquisition_optimizer
-from .gp.gp import kernel,metric,utils
-from .gp.gp.GPR import GPR
 from .bo import BayesOpt
 from .plotter import plot_history
