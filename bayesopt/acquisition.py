6cc62b271fe18cf919554095127144702a04204b
JohnYKiyo <dummn.marionette.7surspecies@gmail.com>
2020-07-17 14:25:14 +0900

    [add] Support for Maximization PI EI

--- bayesopt/acquisition.py
+++ bayesopt/acquisition.py
@@ -5,7 +5,7 @@ import jax.scipy as scp
 from jax import jit
 
 @jit
-def UCB(mu,sigma,kappa=5.):
+def UCB(mu,sigma,kappa=5.,**kwargs):
     return mu + kappa*sigma
 
 @jit
@@ -19,15 +19,34 @@ def scheduledLCB(mu,sigma,kappa=5.,**kwargs):
     return -mu+kappa*schedule*sigma
 
 @jit
-def PI(mu,sigma,xi=0.01,**kwargs):
-    vmin = kwargs.get('vmin')
-    return scp.stats.norm.cdf((vmin-mu-xi)/sigma)
+def scheduledUCB(mu,sigma,kappa=5.,**kwargs):
+    it = kwargs.get('it')
+    schedule = np.sqrt(np.log(it+1) / (it+1))
+    return mu+kappa*schedule*sigma
 
 @jit
-def EI(mu,sigma,**kwargs):
+def MinPI(mu,sigma,xi=0.01,**kwargs):
+    vmin = kwargs.get('vmin')
+    Z =(vmin-mu-xi)/sigma 
+    return scp.stats.norm.cdf(Z)
+
+@jit
+def MaxPI(mu,sigma,xi=0.01,**kwargs):
+    vmax = kwargs.get('vmax')
+    Z = (mu-vmax-xi)/sigma
+    return scp.stats.norm.cdf(Z)
+
+@jit
+def MinEI(mu,sigma,**kwargs):
     vmin = kwargs.get('vmin')
     Z=(vmin-mu)/sigma
-    return np.where(sigma==0,0,(vmin-mu)*scp.stats.norm.cdf(Z)+sigma*scp.stats.norm.pdf(Z))
+    return np.where(sigma==0,0,sigma*(Z*scp.stats.norm.cdf(Z)+scp.stats.norm.pdf(Z)))
+
+@jit
+def MaxEI(mu,sigma,**kwargs):
+    vmax = kwargs.get('vmax')
+    Z=(mu-vmax)/sigma
+    return np.where(sigma==0,0,sigma*(Z*scp.stats.norm.cdf(Z)+scp.stats.norm.pdf(Z)))
 
 def GunbelUCB(mu,sigma,**kwargs):
     raise NotImplementedError
