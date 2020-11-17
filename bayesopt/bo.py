c8f00f00a13ea000a678bb2ba5a28e6dc954b564
JohnYKiyo <dummn.marionette.7surspecies@gmail.com>
2020-11-17 09:37:55 +0000

    [add]Support for nocalculate initY

--- bayesopt/bo.py
+++ bayesopt/bo.py
@@ -1,6 +1,5 @@
 from jax.config import config
 config.update("jax_enable_x64", True)
-
 import jax.numpy as np
 
 from tqdm import tqdm
@@ -24,6 +23,9 @@ class BayesOpt(object):
         acq_optim (function or baysianoptim acquisition_optimizer class):
             Optimizer when searching for the maximum value of the acquisition function.
 
+        initial_Y (array-like): Defaults to None.
+            Y must be the same size as the number of data in X.
+            The i-th index of Y must correspond to the i-th index of X.
         kernel (function or baysianoptim kernel class, optional):
             Kernel used in Gaussian Process Regression. Defaults to GaussKernel(h=1,a=1).
 
@@ -46,17 +48,13 @@ class BayesOpt(object):
         acq (function) : Acquisition function.
     """
 
-    def __init__(self, f, initial_input, acq, acq_optim, kernel=None, alpha=1e-6, maximize=False, function_input_unpacking=True):
+    def __init__(self, f, initial_input, acq, acq_optim, initial_Y=None, kernel=None, alpha=1e-6, maximize=False, function_input_unpacking=True):
 
         self.__objectivefunction = f
         self.__initial_X = transform_data(initial_input)
         self.__function_unpack = function_input_unpacking
-        if self.__function_unpack:
-            self.__initial_Y = self.__objectivefunction(*self.__initial_X.T) ## unpack list (like [x1,x2,...,xd]) to x1,x2,...,xd for function inputs by using '*' operator.
-        else:
-            self.__initial_Y = self.__objectivefunction(self.__initial_X)
-        data_checker(self.__initial_X,self.__initial_Y)
-
+        self.__initial_Y = self.__calculate_initialY(initial_Y)
+        data_checker(self.__initial_X, self.__initial_Y)
         self.__maximize = maximize
 
         # init GPR
@@ -79,6 +77,21 @@ class BayesOpt(object):
         self.__acq = acq
         self.__acq_optimizer = acq_optim
 
+    def __calculate_initialY(self, initial_Y):
+        if initial_Y is None:
+            iniY = []
+            for iniX in self.__initial_X:
+                if self.__function_unpack:
+                    iniY.append(self.__objectivefunction(*iniX.T))  # unpack list (like [x1,x2,...,xd]) to x1,x2,...,xd for function inputs by using '*' operator.
+
+                else:
+                    iniY.append(self.__objectivefunction(iniX))
+            return np.array(iniY)
+        else:
+            if initial_Y.shape[0] != self.__initial_X.shape[0]:
+                raise ValueError('Y must be the same size as the number of data in X.')
+            return transform_data(initial_Y)
+
     def run_optim(self, max_iter, terminate_function=None):
         '''Run baysian optimization.
         Args:
@@ -94,18 +107,19 @@ class BayesOpt(object):
                         else:
                             return False
 
-        '''        
+        '''
+
         with tqdm(total=max_iter) as bar:
             for i in range(max_iter):
                 loc, acq_val = self.__acq_optimizer(gpr=self.__gpr, acq=self.__acq, it=i)
                 if self.__function_unpack:
-                    Y_obs = self.__objectivefunction(*loc.T) ## unpack list (like [x1,x2,...,xd]) to x1,x2,...,xd for function inputs by using '*' operator.
+                    Y_obs = self.__objectivefunction(*loc.T)  # unpack list (like [x1,x2,...,xd]) to x1,x2,...,xd for function inputs by using '*' operator.
                 else:
-                    Y_obs = self.__objectivefunction(np.atleast_2d(loc)) 
+                    Y_obs = self.__objectivefunction(np.atleast_2d(loc))
                 self.__gpr.append_data(np.atleast_2d(loc), Y_obs)
                 self.__X_history.append(loc)
                 self.__Y_history.append(Y_obs)
-                
+
                 if self.__maximize:
                     if self.__best_value is None or Y_obs > self.__best_value:
                         self.__best_value = Y_obs
@@ -114,51 +128,51 @@ class BayesOpt(object):
                     if self.__best_value is None or Y_obs < self.__best_value:
                         self.__best_value = Y_obs
                         self.__best_params = loc
-                
+
                 post_str = f'param:{loc}, value:{Y_obs}, current best param:{self.__best_params}, current best_value:{self.__best_value}'
-                bar.set_description_str(f'BayesOpt')
+                bar.set_description_str('BayesOpt')
                 bar.set_postfix_str(post_str)
                 bar.update()
                 if (terminate_function is not None) and (terminate_function(i, self.__X_history, self.__Y_history)):
                     print(f'break iter:{i}, current best param:{self.__best_params}, current best_value:{self.__best_value}')
                     break
-        
+
     @property
     def param_history(self):
         return np.array(self.__X_history)
-    
+
     @property
     def value_history(self):
         return np.array(self.__Y_history)
-    
+
     @property
     def best_params(self):
         return self.__best_params
-    
+
     @property
     def best_value(self):
         return self.__best_value
-    
+
     @property
     def gpr(self):
         return self.__gpr
-    
+
     @property
     def kernel(self):
         return self.__kernel
-    
+
     @property
     def alpha(self):
         return self.__alpha
-        
+
     @property
     def maximization(self):
         return self.__maximize
-    
+
     @property
     def n_trial(self):
         return len(self.__Y_history)
-    
+
     @property
     def acq(self):
         return self.__acq
